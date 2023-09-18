81122fdc5c821f7b31068383f53a8fb9f62b71be
Favio Valero <favio102@protonmail.com>
2023-09-18 04:51:48 -0700

    axios, rspec, modified

--- app/controllers/api/users_controller.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-module Api
-  class UsersController < ApplicationController
-    def show
-      
-    end
-  end
-end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,6 @@ Rails.application.routes.draw do
   root "home#index"
 
   namespace :api do
-    resources :users, only: :show
+    get "/users_by_email/:email" => "users_by_emails#show", as: :users_by_email
   end
 end
