7d4b6669cb30fda873ac68ede80cfd032c3a75e7
Favio Valero <favio102@protonmail.com>
2023-09-22 03:35:05 -0700

    geolib, distance calculation added

--- app/javascript/controllers/geolocation_controller.js
+++ app/javascript/controllers/geolocation_controller.js
@@ -1,18 +1,22 @@
-import { Controller } from '@hotwired/stimulus'
-import { getDistance, convertDistance } from 'geolib';
+import { Controller } from '@hotwired/stimulus';
+import { getPreciseDistance, convertDistance } from 'geolib';
 import { isEmpty } from 'lodash-es';
 
 export default class extends Controller {
   static targets = ['property'];
 
   connect() {
-    // if the controller doesn't have the user's geolocation, it gets the browser's geolocation
+    // If the controller doesn't have the user's geolocation, it gets the browser's geolocation
     if (isEmpty(this.element.dataset.latitude) && isEmpty(this.element.dataset.longitude)) {
-      window.navigator.geolocation.getCurrentPosition((position) => {
-        this.setUserCoordinates(position.coords);
-        this.setDistanceText()
-      });
-      // if the controller have the user's geolocation
+      window.navigator.geolocation.getCurrentPosition(
+        (position) => {
+          this.setUserCoordinates(position.coords);
+          this.setDistanceText();
+        },
+        (error) => {
+          console.error('Error getting geolocation:', error);
+        }
+      );
     } else {
       this.setDistanceText();
     }
@@ -25,19 +29,31 @@ export default class extends Controller {
 
   getUserCoordinates() {
     return {
-      latitude: this.element.dataset.latitude,
-      longitude: this.element.dataset.longitude,
+      latitude: parseFloat(this.element.dataset.latitude), // Parse as float to ensure numeric values
+      longitude: parseFloat(this.element.dataset.longitude), // Parse as float to ensure numeric values
     };
   }
 
   setDistanceText() {
     this.propertyTargets.forEach((propertyTarget) => {
-      let distanceFrom = getDistance(
-        this.getUserCoordinates(),
-        { latitude: propertyTarget.dataset.latitude, longitude: propertyTarget.dataset.longitude },
-      );
+      const targetLatitude = parseFloat(propertyTarget.dataset.latitude);
+      const targetLongitude = parseFloat(propertyTarget.dataset.longitude);
 
-      propertyTarget.querySelector('[data-distance-away]').innerHTML = `${Math.round(convertDistance(distanceFrom, 'km'))} kilometers away`;
+      // Check if latitude and longitude are valid numbers
+      if (!isNaN(targetLatitude) && !isNaN(targetLongitude)) {
+        const distanceFrom = getPreciseDistance(this.getUserCoordinates(), {
+          latitude: targetLatitude,
+          longitude: targetLongitude,
+        });
+
+        propertyTarget.querySelector('[data-distance-away]').innerHTML = `${Math.round(
+          convertDistance(distanceFrom, 'km')
+        )} kilometers away`;
+      } else {
+        // Handle the case where latitude or longitude is not a valid number
+        propertyTarget.querySelector('[data-distance-away]').innerHTML = 'Distance unavailable';
+        console.error('Invalid latitude or longitude values:', targetLatitude, targetLongitude);
+      }
     });
   }
 }
