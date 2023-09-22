efc1fb8910ca21bc7bf4e6eb27b7d004590e7c83
Favio Valero <favio102@protonmail.com>
2023-09-22 10:21:19 -0700

    seeds updated

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,16 +1,25 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 
 puts "Creating Properties...."
-10.times do
-  Property.create!(
-    name: Faker::Lorem.word,
-    headline: Faker::Lorem.sentence,
-    description: Faker::Lorem.paragraph,
+10.times do |i|
+  property = Property.create!(
+    name: Faker::Lorem.unique.word,
+    headline: Faker::Lorem.unique.sentence,
+    description: Faker::Lorem.paragraphs(number: 30).join(" "),
     address_1: Faker::Address.street_address,
     city: Faker::Address.city,
     state: Faker::Address.state,
-    country: 'United States'
-    # price_cents: (30..400).to_a.sample
+    country: "United States",
+    price: Money.from_amount((25..100).to_a.sample)
+
   )
+  puts "Properties seeds created!!"
+  puts "Creating images....."
+  property.images.attach(io: File.open(Rails.root.join("db","sample","images","home_#{i + 1}.jpg")), filename: property.name)
+  puts "Images created!"
+
+  # (1..5).to_a.sample.times do
+  #   Review.create(reviewable: property, rating: (1..5).to_a.sample, title: Faker::Lorem.word, body: Faker::Lorem.paragraph, user: User.all.sample)
+  # end
+
 end
-puts "Properties seeds created!!"
