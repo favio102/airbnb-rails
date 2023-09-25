f41171145a7e8bfa233781a274ab281f7a538e36
Favio Valero <favio102@protonmail.com>
2023-09-25 01:47:09 -0700

    user review added

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_23_175232) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_25_073401) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -108,7 +108,9 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_23_175232) do
     t.string "reviewable_type"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
     t.index ["reviewable_id", "reviewable_type"], name: "index_reviews_on_reviewable_id_and_reviewable_type"
+    t.index ["user_id"], name: "index_reviews_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -119,6 +121,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_23_175232) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "first_name"
+    t.string "last_name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
@@ -130,4 +134,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_23_175232) do
   add_foreign_key "profiles", "users"
   add_foreign_key "reservations", "properties"
   add_foreign_key "reservations", "users"
+  add_foreign_key "reviews", "users"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,32 @@
 # This file should contain all the record creation needed to seed the database with its default values.
+puts 'Cleaning data base...................'
+Review.destroy_all
+User.destroy_all
+Reservation.destroy_all
+Property.destroy_all
+Profile.destroy_all
+Favorite.destroy_all
+puts 'Database cleaned!!'
+
 puts 'Preparing data seeds..........'
+
+puts 'Creating user images'
+user_pictures = []
+6.times do
+  user_pictures << URI.parse(Faker::LoremFlickr.image).open
+end
+puts 'User images created!'
+
+puts 'Creating users..........'
+me = User.create(email: "test@pe.me", first_name: "Jeremias", last_name: "Sprinfield", password: "wwweeerrr")
+me.picture.attach(io: user_pictures[0], filename: "#{me.full_name}.jpg")
+
+5.times do |i|
+  user = User.create(email: Faker::Internet.email, first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, password: "password")
+  user.picture.attach(io: user_pictures[i + 1], filename: "#{user.full_name}.jpg")
+end
+puts 'Users data created!'
+
 puts 'Creating Properties....'
 11.times do |i|
   property = Property.create!(
@@ -12,15 +39,12 @@ puts 'Creating Properties....'
     country: 'United States',
     price: Money.from_amount((25..100).to_a.sample)
   )
-  puts 'Properties seeds created!!'
-  puts 'Creating images.....'
+  puts 'Properties data created!!'
   property.images.attach(io: File.open(Rails.root.join('db', 'sample', 'images', "home_#{i + 1}.jpg")), filename: property.name)
-  puts 'Images created!'
-
   puts 'Creating reviews ....'
   (1..5).to_a.sample.times do
-    Review.create(reviewable: property, rating: (1..5).to_a.sample, title: Faker::Lorem.word, body: Faker::Lorem.paragraph)
+    Review.create(reviewable: property, rating: (1..5).to_a.sample, title: Faker::Lorem.word, body: Faker::Lorem.paragraph, user: User.all.sample)
   end
   puts 'Reviews created!!!'
-  puts 'Seed completed successfully'
 end
+puts 'Seed completed successfully'
