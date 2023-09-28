b18d9c530caddfdfb1068302f22062a2a81f1393
Favio Valero <favio102@protonmail.com>
2023-09-28 06:56:43 -0700

    stripe configuration

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -2,6 +2,8 @@ class Reservation < ApplicationRecord
   belongs_to :property
   belongs_to :user
 
+  has_one :payment, dependent: :destroy
+
   validates :checkin_date, presence: true
   validates :checkout_date, presence: true
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,7 @@ class User < ApplicationRecord
   has_one_attached :picture
   has_many :favorites, dependent: :destroy
   has_many :favorited_properties, through: :favorites, source: :property
+  has_many :payments, through: :reservations
   has_many :reservations, dependent: :destroy
   has_many :reserved_properties, through: :reservations, source: :property
   has_many :reviews, dependent: :destroy
