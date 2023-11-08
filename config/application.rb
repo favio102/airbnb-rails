68d3b96773c0cdde9030d7081fb1e65c2a794096
Favio Valero <favio102@protonmail.com>
2023-11-07 19:40:16 -0800

    add ->  config.assets.css_compressor = nil to application.rb to fix asset pipeline SassC::SyntaxError with Tailwind

--- config/application.rb
+++ config/application.rb
@@ -30,6 +30,8 @@ module AirbnbRails
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    # To fix Rails 7 asset pipeline SassC::SyntaxError with Tailwind
+    config.assets.css_compressor = nil
 
     # Don't generate system test files.
     config.generators.system_tests = nil
